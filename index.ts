40d41b53e74ef885df7a8a1f2827171b3ca94ac3
Satish Konduru <satishkonduru0225@gmail.com>
2022-06-23 19:27:37 +0530

    Initial Commit

---
+++ index.ts
@@ -0,0 +1,28 @@
+export{}
+// console.log("Satish")
+// boolean, number, string > Primitive Datatypes
+// let isFresher: boolean = true
+// let total  : number
+// let name: string = "Nagarjuna"
+
+
+// let statement = `my Name is ${name}
+//  and I am  a fresher to Angular.
+//  From today onwards I am learning Angular`
+// console.log(statement)
+
+// let x : number | boolean | string
+// x = 10
+// x = false
+// x = "Siva"
+
+// let y : any
+// y = 10
+// y = false
+// y = "Siva"
+
+
+
+
+
+
