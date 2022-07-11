4ea5e199771f2eabcc0780a631dd993890c18e09
Satish Konduru <satishkonduru0225@gmail.com>
2022-07-12 00:42:08 +0530

    functions

--- index.js
+++ index.js
@@ -1,6 +1,45 @@
 "use strict";
 exports.__esModule = true;
-//string, boolean, number
-var name = 'Satish';
-var statement = "Hi, \nMy name is ".concat(name, ". \nNow I am learning Angular. \nI am single");
-console.log(statement);
+//string, boolean, number, null, undefined
+// let name = 'Satish'
+// let statement = `Hi, 
+// My name is ${name}. 
+// Now I am learning Angular. 
+// I am single`
+// console.log(statement)
+// let x : null = null
+// let y : undefined = undefined
+// let isFresher: boolean = null
+// let total: number = undefined
+// total = 100
+// let list1: number[] = [10,20,30,40]
+// let list2: Array<boolean>  = [true, true, false, false, true]
+// let p1: [string, number, boolean] = ['Satish', 43, true]
+// let p2: [number, boolean, string]  = [20, true, 'SK']
+function add(x, y) {
+    if (y === void 0) { y = 100; }
+    if (y) {
+        return (x + y);
+    }
+    else {
+        return x;
+    }
+}
+var mysum = add(10); // actual parameter
+console.log(mysum);
+// function sub(a, b){
+//   return  a-b
+// }
+// let substation = sub(100, 50) // let substation = 50
+// let num = 100 + substation
+var x = 10;
+var list = [10, 20];
+var person = {
+    firstName: 'Satish',
+    lastName: 'Konduru',
+    age: 43
+};
+function fullName(myInterface) {
+    console.log(myInterface.firstName);
+}
+fullName(person);
--- index.ts
+++ index.ts
@@ -1,12 +1,55 @@
 export{}
-//string, boolean, number
+//string, boolean, number, null, undefined
 
-let name = 'Satish'
-let statement = `Hi, 
-My name is ${name}. 
-Now I am learning Angular. 
-I am single`
+// let name = 'Satish'
+// let statement = `Hi, 
+// My name is ${name}. 
+// Now I am learning Angular. 
+// I am single`
 
-console.log(statement)
+// console.log(statement)
+
+// let x : null = null
+// let y : undefined = undefined
+// let isFresher: boolean = null
+// let total: number = undefined
+// total = 100
+// let list1: number[] = [10,20,30,40]
+// let list2: Array<boolean>  = [true, true, false, false, true]
+
+// let p1: [string, number, boolean] = ['Satish', 43, true]
+// let p2: [number, boolean, string]  = [20, true, 'SK']
+function add(x: number, y: number = 100){
+   if(y){
+       return (x+y)
+   }
+   else{
+    return x
+   }
+    
+}
+let mysum = add(10) // actual parameter
+console.log(mysum)
+// function sub(a, b){
+//   return  a-b
+// }
+
+// let substation = sub(100, 50) // let substation = 50
+// let num = 100 + substation
 
 
+let x = 10
+let list = [10,20]
+let person = {
+    firstName: 'Satish',
+    lastName: 'Konduru',
+    age: 43
+}
+interface myInterface{
+    firsName: string
+}
+function fullName(myInterface){
+    console.log(myInterface.firstName)
+}
+
+fullName(person)
